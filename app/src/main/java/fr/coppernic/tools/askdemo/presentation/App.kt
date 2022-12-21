d94f246bc99b39f37d9f4291185f6fc927250406
Damien Praca <damien.praca@coppernic.fr>
2022-12-21 19:39:19 +0100

    Display timber logs in debug mode

--- app/src/main/java/fr/coppernic/tools/askdemo/presentation/App.kt
+++ app/src/main/java/fr/coppernic/tools/askdemo/presentation/App.kt
@@ -3,8 +3,10 @@ package fr.coppernic.tools.askdemo.presentation
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.LifecycleObserver
+import fr.coppernic.tools.askdemo.BuildConfig
 import fr.coppernic.tools.askdemo.presentation.di.AppComponent
 import fr.coppernic.tools.askdemo.presentation.di.AppModule
+import timber.log.Timber
 
 
 class App : Application(), LifecycleObserver {
@@ -16,6 +18,9 @@ class App : Application(), LifecycleObserver {
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         components = AppModule(this)
     }
